53e85dd7a96ac98ad5f8a8361c4a800b41141ed0
tanya-java <termalickaya@gmail.com>
2021-07-08 10:24:38 +0300

    Heroku Tests

---
+++ src/test/java/HerokuTests/FileUploadTest.java
@@ -0,0 +1,29 @@
+package HerokuTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class FileUploadTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\TeachMeSkills\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void upload(){
+        driver.get("http://the-internet.herokuapp.com/upload");
+        By fileUpload = By.cssSelector("upload[type=file]");
+        String filePath = "/c/hp/desktop/unnamed.png";
+        driver.findElement(fileUpload).sendKeys(filePath);
+
+    }
+}
---
+++ src/test/java/HerokuTests/HerokuTests.java
@@ -0,0 +1,40 @@
+package HerokuTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HerokuTests {
+    WebDriver driver;
+    private static final String ALERT_TEXT = "You selected a context menu";
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\HP\\Downloads\\TeachMeSkills\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void getAlertTextTest() {
+        driver.get("http://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.id("hot-spot"))).contextClick().build().perform();
+        String alertText = driver.switchTo().alert().getText();
+        driver.switchTo().alert().accept();
+        Assert.assertTrue(driver.findElement(By.id("content")).isDisplayed(), "Alert is still opened");
+        Assert.assertEquals(alertText, ALERT_TEXT, "Texts are not equals");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+}
